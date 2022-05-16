a470c3fd74cfd5ca8ee6c47546353a2f940382b3
Mark Witt <hello@markwitt.me>
2022-05-16 15:23:54 +0200

    start ex2 and add validation of jewels being next to each other to ex1

--- 2/app.js
+++ 2/app.js
@@ -1,10 +1,17 @@
 const readline = require("node:readline/promises");
 const { stdin: input, stdout: output } = require("process");
 
-const board = [5, 2, 3, 5, 4, 5, 0, 5, 4];
+// prettier-ignore
+const board = 
+[
+ 2, 1, 2, 3,
+ 5, 4, 4, 5,
+ 0, 5, 3, 4,
+ 6, 7, 6, 1
+];
 
-const getRow = (i) => board.slice(i * 3, i * 3 + 3);
-const getCol = (i) => [board[i], board[i + 3], board[i + 6]];
+const getRow = (i) => board.slice(i * 4, i * 4 + 4);
+const getCol = (i) => [board[i], board[i + 4], board[i + 8]];
 
 validateLine = (line) => line.every((el) => el === line[0]);
 
@@ -24,14 +31,14 @@ const validateBoard = () => {
   return console.log("INVALID");
 };
 
-const between0And8 = (n) => !isNaN(n) && n >= 0 && n <= 8;
+const between0And15 = (n) => !isNaN(n) && n >= 0 && n <= 15;
 
 const isDir = (s) => ["l", "u", "r", "d"].includes(s);
 const dirToN = {
   l: -1,
-  u: -3,
+  u: -4,
   r: +1,
-  d: +3,
+  d: +4,
 };
 
 const rl = readline.createInterface({ input, output });
@@ -42,14 +49,14 @@ const runGame = async () => {
 
   let first;
 
-  while (!between0And8(first)) {
+  while (!between0And15(first)) {
     const str = await rl.question("Enter the position of the first jewel:");
     first = parseInt(str);
   }
 
   let second;
 
-  while (!between0And8(second)) {
+  while (!between0And15(second)) {
     const str = await rl.question(
       "Enter the position of the second jewel or a direction (l | u | r | d):"
     );
@@ -58,6 +65,7 @@ const runGame = async () => {
       second = first + dirToN[str];
     }
   }
+
   const diff = first - second;
 
   if (Object.values(dirToN).includes(diff)) {
